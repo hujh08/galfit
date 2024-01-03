af888412c9fd1b65b75e00fa6baf00a2d1947e07
Jh Hu <hu.jianhong_2008@163.com>
2024-01-03 16:57:36 +0800

    feat (mask): flexible args for reg mask

--- mask/mask.py
+++ mask/mask.py
@@ -64,6 +64,9 @@ class RegMask:
     def get_mask(self, hdu=None, header=None, shape=None):
         '''
             return bool array with masked pixel True
+
+            if header given
+                shape is also needed
         '''
         if all([k is None for k in [hdu, header, shape]]):
             hdu=self._hdu
@@ -71,6 +74,30 @@ class RegMask:
         return self._shapes\
                    .get_mask(hdu=hdu, header=header, shape=shape)
 
+    def get_mask_by_header(self, header, shape=None):
+        '''
+            create mask array by header
+        '''
+        if shape is None:
+            shape=(header['NAXIS2'], header['NAXIS1'])
+
+        return self.get_mask(header=header, shape=shape)
+
+    def get_mask_by_wcs(self, wcs, shape):
+        '''
+            create mask array by WCS object and shape
+        '''
+        header=wcs.to_header()
+
+        # add NAXISi
+        ny, nx=shape
+        header['NAXIS']=2
+        header['NAXIS1']=nx
+        header['NAXIS2']=ny
+
+        return self.get_mask(header=header, shape=shape)
+
+    ## save to fits
     def write_gf_mask(self, path, overwrite=True, **kwargs):
         '''
             write mask used in galfit
